158323c62d4126397d90f0f5bc1c09a9d80bbd84
Lmarl Saria <lmarlsaria21@gmail.com>
2023-04-18 14:47:17 +0800

    fix

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -42,11 +42,8 @@ public class Finance {
                 System.out.println("Finding your monthly payment ...");
                 MortgageCalculator.main(arguments);
                 return;
-            case "LaunchRocketToMoon":
-                System.out.println("Launching rocket to the moon...");
-                return;
             default:
-                System.out.println(COMMAND_NOT_FOUND);
+                System.out.println(command + ": command not found");
                 return;
         }
     }
