bbe776411f07c3ad55f6140253a020efa14d65c5
Lmarl Saria <lmarlsaria21@gmail.com>
2023-04-18 13:17:54 +0800

    update

--- src/main/java/com/h2/BestLoanRate.java
+++ src/main/java/com/h2/BestLoanRate.java
@@ -1,13 +1,7 @@
 package com.h2;
 
 
-
 public class BestLoanRate {
-    
-            if (bestRates.containsKey(loanTermInYears)) {
-                return bestRates.get(loanTermInYears);
-            } else {
-                return 0.0f;
-            }
-        }
+    public static void main(String args[]){
+
 }
